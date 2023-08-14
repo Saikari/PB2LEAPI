5ac75e2cc1e40139c530f0626cad1a21827bf80c
さくらみこ <1145793@edu.rut-miit.ru>
2023-08-14 14:34:09 +0300

    Add files via upload
    Optimization and code fix.
    1 Class fix usage and parsing maps algorithms
    2 Kwargs for classes
    3 New classes were added

--- DataInterfaces/Entity.py
+++ DataInterfaces/Entity.py
@@ -1,19 +1,111 @@
-# Map Object Without Name
-class BaseMapObject:
+"""
+Entity Module
+
+This module defines classes representing various game entities and actions within a game.
+It includes classes for nameless and named map object entities, image entities,
+and trigger actions. Additionally, it defines a core action used in PB2 Triggers.
+
+Classes:
+    NamelessMapObjectEntity:
+        Represents a nameless map object entity.
+
+    ImageEntity:
+        Represents an image entity.
+
+    NamedMapObjectEntity:
+        Represents a named map object entity, inheriting from NamelessMapObjectEntity.
+
+    TriggersActionEntity:
+        Represents an action within a trigger.
+
+Constants:
+    DO_NOTHING:
+        A core action used in PB2 Triggers that represents doing nothing.
+
+Usage Example:
+    # Import the classes and constant
+    from game_entities import (
+        NamelessMapObjectEntity,
+        ImageEntity,
+        NamedMapObjectEntity,
+        TriggersActionEntity,
+        DO_NOTHING,
+    )
+
+    # Create instances of the defined classes
+    nameless_entity = NamelessMapObjectEntity(x=10, y=20)
+    image = ImageEntity(width=100, height=200, id=1)
+    named_entity = NamedMapObjectEntity(x=30, y=40, uid="entity_123")
+    action = TriggersActionEntity(opID=5, args=[arg1, arg2])
+
+    # Use the DO_NOTHING action
+    trigger_action = DO_NOTHING
+"""
+
+
+class NamelessMapObjectEntity:
+    """
+    Represents a nameless map object entity.
+
+    Attributes:
+        x (int): The X-coordinate of the map object.
+        y (int): The Y-coordinate of the map object.
+    """
+
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
+    def print_all_fields(self):
+        print(", ".join([f"{key}: {value}" for key, value in self.__dict__.items()]))
 
-# Map Object With Name
-class BaseNamedMapObject(BaseMapObject):
+
+class ImageEntity:
+    """
+    Represents an image entity.
+
+    Attributes:
+        width (int): The width of the image.
+        height (int): The height of the image.
+        id (int): The ID of the image.
+    """
+
+    def __init__(self, width=0, height=0, id=0):
+        self.width = width
+        self.height = height
+        self.id = id
+
+    def print_all_fields(self):
+        print(", ".join([f"{key}: {value}" for key, value in self.__dict__.items()]))
+
+
+class NamedMapObjectEntity(NamelessMapObjectEntity):
+    """
+    Represents a named map object entity, inheriting from NamelessMapObjectEntity.
+
+    Attributes:
+        x (int): The X-coordinate of the map object.
+        y (int): The Y-coordinate of the map object.
+        uid (str): The unique identifier of the map object.
+    """
+
     def __init__(self, x=0, y=0, uid=""):
         super().__init__(x, y)
         self.uid = uid
 
+    def print_all_fields(self):
+        print(", ".join([f"{key}: {value}" for key, value in self.__dict__.items()]))
 
-# Triggers Action, 1 Trigger consists of 10 Actions
-class Action:
+
+class TriggersActionEntity:
+    """
+    Represents an action within a trigger.
+
+    Attributes:
+        opID (int): The operation ID of the action.
+        args (list): The list of arguments for the action.
+    """
+
     def __init__(self, opID=0, args=None):
         if args is None:
             args = []
@@ -22,4 +114,4 @@ class Action:
 
 
 # Most used Action at PB2 Triggers Literally the core of whole game
-DO_NOTHING = Action(opID=-1, args=[])
+DO_NOTHING = TriggersActionEntity(opID=-1, args=[])
--- main.py
+++ main.py
@@ -1,11 +1,63 @@
-import logging
+"""
+Asynchronous Map Loading and Dumping Example
+
+This script demonstrates asynchronous loading and dumping of map instances using the MapIO class.
+It uses asyncio and logging to showcase how to load a map from a file, modify it, and then asynchronously
+dump it back to a file.
+
+Usage:
+    - Ensure you have the MapIO module with the required functions.
+    - Make sure to have the 'XML/bossfight_map1.xml' file for loading.
+    - The modified map instance is then dumped into 'output.xml'.
+
+Example Usage:
+    # Import required modules
+    from asyncio import run
+    from logging import basicConfig, error, DEBUG, info
+    from DataInterfaces.MapIO import MapIO
+
+    # Set up logging configuration
+    basicConfig(
+        level=DEBUG,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    # Example usage
+    async def main():
+        try:
+            map_instance = await MapIO.load_map_from_file_async("XML/bossfight_map1.xml")
+            info("Map loaded successfully!")
+            info("Map instance has been created successfully!")
+            # Assuming you have a map_instance created and modified
+            await MapIO.dump_map_async(map_instance, "output.xml")
+            info("Map dumped and written to file asynchronously!")
+
+        except Exception as e:
+            error(f"Error: {e}", exc_info=True)
+
+    if __name__ == "__main__":
+        run(main())
+"""
+
+# This script demonstrates asynchronous loading and dumping of map instances using the MapIO class.
+# It uses asyncio and logging to showcase how to load a map from a file, modify it, and then asynchronously
+# dump it back to a file.
+
+# Usage:
+#     - Ensure you have the MapIO module with the required functions.
+#     - Make sure to have the 'XML/bossfight_map1.xml' file for loading.
+#     - The modified map instance is then dumped into 'output.xml'.
+
 from asyncio import run
+from logging import basicConfig, error, DEBUG, info
 
 from DataInterfaces.MapIO import MapIO
+from DataInterfaces.MapObjects import Player
 
 # Set up logging configuration
-logging.basicConfig(
-    level=logging.DEBUG,
+basicConfig(
+    level=DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
@@ -13,18 +65,41 @@ logging.basicConfig(
 
 # Example usage
 async def main():
+    """
+    Asynchronous Map Loading and Dumping Entry Point
+
+    This function serves as the entry point for an asynchronous process of loading a map
+    from a file, modifying it, and then asynchronously dumping it back to a file.
+    It demonstrates the usage of the MapIO class with async operations and logging.
+
+    Raises:
+        Exception: If an error occurs during map loading, modification, or dumping.
+
+    Example Usage:
+        # Import required modules
+        from asyncio import run
+        from logging import error, info
+        from DataInterfaces.MapIO import MapIO
+
+        # Example usage
+        if __name__ == "__main__":
+            try:
+                run(main())
+            except Exception as e:
+                error(f"Error: {e}", exc_info=True)
+    """
     try:
         map_instance = await MapIO.load_map_from_file_async("XML/bossfight_map1.xml")
-        logging.info("Map loaded successfully!")
-        logging.debug(map_instance)  # Use logging.debug for more detailed information
-        logging.info("Map instance has been created successfully!")
-
+        info("Map loaded successfully!")
+        info("Map instance has been created successfully!")
+        for i in map_instance.objects[Player]:
+            i.print_all_fields()
         # Assuming you have a map_instance created and modified
         await MapIO.dump_map_async(map_instance, "output.xml")
-        logging.info("Map dumped and written to file asynchronously!")
+        info("Map dumped and written to file asynchronously!")
 
     except Exception as e:
-        logging.error(f"Error: {e}", exc_info=True)
+        error(f"Error: {e}", exc_info=True)
 
 
 if __name__ == "__main__":
